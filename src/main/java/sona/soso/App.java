637b5a48870dedcec8e576de670070f5d6a79a27
kiran Ganta <kkraj422@gmail.com>
2020-09-21 09:03:02 +0530

    added helloo

---
+++ src/main/java/sona/soso/App.java
@@ -0,0 +1,32 @@
+package sona.soso;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        //System.out.println( "Hello World!" );
+    	//System.out.println("helllllllllllllllo worlddddddddddddd");
+    	
+    	Vector a = new Vector();
+    	
+    	a.add("Kran");
+
+    	a.add("Ashok");
+    	
+    	ArrayList al = new ArrayList(a);
+    	
+    	System.out.println(" vecotor "+al);
+
+    	String [] params = new String[] {"abc","1-1-2022"};
+    	
+    	//System.out.println(params[0]);
+    	
+    }
+}
